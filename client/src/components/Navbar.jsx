0087fccf287d18db1a52385418bdc9bf2c2d73f8
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-07-16 23:56:34 +0530

    Add logic to toggle sidebar from navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ import {
   useTheme,
 } from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
@@ -33,7 +33,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
