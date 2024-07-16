2585cd68204a2018a4c640a3e8ef1b5b537fd7cc
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-07-17 00:16:33 +0530

    Add data to MongoDB database

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -36,5 +39,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ONLY ADD DATA ONE TIME */
+    User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
