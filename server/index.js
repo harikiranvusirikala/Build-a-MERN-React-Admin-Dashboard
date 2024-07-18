81ebb984c57508b1ad5c09854da03460fb6fe50d
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-07-18 22:32:44 +0530

    Add Product, ProductStat data to MongoDB database

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -42,5 +45,7 @@ mongoose
 
     /* ONLY ADD DATA ONE TIME */
     // User.insertMany(dataUser);
+    Product.insertMany(dataProduct);
+    ProductStat.insertMany(dataProductStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
