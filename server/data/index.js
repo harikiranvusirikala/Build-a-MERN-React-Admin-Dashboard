a5d680d77d3098702b1e1b3e592b767b4451f960
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-08-05 21:30:40 +0530

    Change profile photo, displayed user details

--- server/data/index.js
+++ server/data/index.js
@@ -18041,13 +18041,13 @@ export const dataUser = [
   },
   {
     _id: "63701cc1f03239b7f700000e",
-    name: "Shelly",
+    name: "Hari",
     email: "swelbeck12@ycombinator.com",
     password: "RSjzmAjnq",
     city: "Sangoleng",
     state: null,
     country: "ID",
-    occupation: "Pharmacist",
+    occupation: "Programmer",
     phoneNumber: "7036619983",
     transactions: [
       "63701d74f03239d81e000027",
